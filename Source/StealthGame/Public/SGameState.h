9a34b4f0f719e18150f3fb6bb99550928766e258
2445968904 <44425297+2445968904@users.noreply.github.com>
2023-12-09 23:46:02 +0800

    FirstUpdate

---
+++ Source/StealthGame/Public/SGameState.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEALTHGAME_API USGameState : public UObject
+{
+	GENERATED_BODY()
+};
---
+++ Source/StealthGame/Public/SPlayerState.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEALTHGAME_API USPlayerState : public UObject
+{
+	GENERATED_BODY()
+};
